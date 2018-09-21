88d6ab63e24e2720c27f1c36adc39d76f94de2b1
Thomas Harmeyer <thomasmharmeyer@gmail.com>
2018-09-21 11:55:03 -0500

    Added some stuff

--- Percolate.java
+++ Percolate.java
@@ -1,4 +1,4 @@
-public class Percolate
+public class Percolate {
 	public PercolateHelp(int [] field){
 		for(int x = 0; x < field.length; x++){
 			if(field[x][0]==0){
@@ -7,8 +7,23 @@ public class Percolate
 			}
 		}
 	}
-	
+
 	public PercolateRE(int [] field){
-		for (int x = 0;
+		int xCord = 0;
+		int yCord = 0;
+		Out:for (int x = 0; x < field.length; x++){
+			for (int y = 0; y < field.length; y++){
+				if(field[x][y]==4){
+					xCord=x;
+					yCord=y;
+					break Out;
+				}
+			}
+		}
+		if((y!=0) && (field[xCord][yCord+1]==0){
+			field[xCord][yCord+1]=4;
+			field[xCord][yCord]=3;
+			PercolateRE(field)
+		}
 	}
 }
